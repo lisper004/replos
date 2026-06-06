8798d21dfad8427cbcaf2e4ea3465be1f5a59431
Lisper004 <meruniaiharu@gmail.com>
2026-06-06 14:12:25 +0500

    updated file.scm

--- core/autoload/file.scm
+++ core/autoload/file.scm
@@ -1,3 +1,5 @@
+;; More Utils
+
 (define (why)
   (simple-format #t "why?\n")
   (simple-format #t "idk bro\n"))
@@ -5,3 +7,21 @@
 (define (helloworld)
   (display "hello world!")
   (newline))
+
+(define (cmd arg)
+  (system (string-append arg)))
+
+(define (git-status)
+  (cmd "git status"))
+
+(define (git-add-all)
+  (cmd "git add ."))
+
+(define (git-commit-m aboutcommit)
+  (cmd (simple-format #f "git commit -m \"~a\"" aboutcommit)))
+
+(define (git-pull-r)
+  (cmd "git pull --rebase"))
+
+(define (git-push)
+  (cmd "git push"))
